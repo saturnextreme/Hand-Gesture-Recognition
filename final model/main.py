8c28e3b3961501ea0c957d3eb39d10911762b84f
Aashay Metekar <97591189+saturnextreme@users.noreply.github.com>
2024-05-12 12:31:35 +0530

    Add files via upload

---
+++ final model/main.py
@@ -0,0 +1,92 @@
+import cv2
+import numpy as np
+import os
+from matplotlib import pyplot as plt
+import time 
+import mediapipe as mp
+import tensorflow
+
+
+mp_holistic = mp.solutions.holistic
+mp_drawing = mp.solutions.drawing_utils
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False
+    results = model.process(image)
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    return image, results
+
+def draw_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+
+def draw_styled_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                             mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4),
+                             mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2))
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                             mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
+                             mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2))
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                             mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
+                             mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2))  
+
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] 
+                     for res in results.pose_landmarks.landmark]
+                   ).flatten() if results.pose_landmarks else np.zeros(33*4)
+    lh = np.array([[res.x, res.y, res.z] 
+                   for res in results.left_hand_landmarks.landmark]
+                   ).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] 
+                   for res in results.right_hand_landmarks.landmark]
+                   ).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+    return np.concatenate([pose, lh, rh])
+
+
+
+model = tensorflow.keras.models.load_model('emotion.h5')
+
+actions = np.array(['hello', 'thanks', 'iloveyou', 'bye', 'sorry', 'ok'])
+no_sequences = 30
+sequence_length = 30
+
+sequence = []
+sentence = []
+threshold = 0.8
+
+colors = [(255, 192, 192), (144, 238, 144), (135, 206, 250), (255, 192, 203), (128, 0, 128), (255, 255, 0)]
+def prob_viz(res, actions, input_frame, colors):
+    output_frame = input_frame.copy()
+    for num, prob in enumerate(res):
+        cv2.rectangle(output_frame, (0,60+num*40), (int(prob*100), 90+num*40), colors[num], -1)
+        cv2.putText(output_frame, actions[num], (0, 85+num*40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+        
+    return output_frame
+
+cap = cv2.VideoCapture(0)
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        image, results = mediapipe_detection(frame, holistic)
+        draw_styled_landmarks(image, results)
+        
+        keypoints = extract_keypoints(results)
+        sequence.append(keypoints)
+        sequence = sequence[-30:]
+        
+        if len(sequence) == 30:
+            res = model.predict(np.expand_dims(sequence, axis=0))[0]
+#             print(actions[np.argmax(res)])
+                
+            image = prob_viz(res, actions, frame, colors)
+        
+        cv2.imshow('OpenCV Feed', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
